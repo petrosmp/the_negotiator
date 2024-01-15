07c12d4cd4b66de7e9cfb7de7af281a362bf69fa
petrosmp <petrakis798@gmail.com>
2024-01-15 11:31:34 +0200

    Training dataset for domains 0-15

---
+++ ANL-2023-example-agent/agents/the_negotiator/training_data/dataset_tournament_kalo_20240115-02_56_18.py
@@ -0,0 +1,58 @@
+results = [
+	{
+		"features": {'avg_bid_util': 0.30639243190454546, 'avg_vals_per_issue': 10.75, 'bid_util_std_dev': 0.24595463575871232, 'num_of_bids': 5808, 'num_of_issues': 4, 'weight_std_dev': 0.24820415326903777},
+		"results":{'Agent007': 0.4932890358399999, 'DreamTeam109Agent': 0.49774392322, 'TemplateAgent': 0.34509031858, 'GEAAgent': 0.41982128098000004, 'Agent33': 0.59939036481},
+	},
+	{
+		"features": {'avg_bid_util': 0.324198643975, 'avg_vals_per_issue': 6.8, 'bid_util_std_dev': 0.1838732608439996, 'num_of_bids': 4608, 'num_of_issues': 5, 'weight_std_dev': 0.1575921617340152},
+		"results":{'Agent007': 0.77664342704, 'DreamTeam109Agent': 0.8479992188200001, 'TemplateAgent': 0.8274702099000001, 'GEAAgent': 0.8182829765000001, 'Agent33': 0.84187613586},
+	},
+	{
+		"features": {'avg_bid_util': 0.324633125155, 'avg_vals_per_issue': 7.4, 'bid_util_std_dev': 0.1867004548806813, 'num_of_bids': 6480, 'num_of_issues': 5, 'weight_std_dev': 0.14990002828552101},
+		"results":{'Agent007': 0.6112385678699999, 'DreamTeam109Agent': 0.59904248085, 'TemplateAgent': 0.5226426456100001, 'GEAAgent': 0.47432990369, 'Agent33': 0.54787868552},
+	},
+	{
+		"features": {'avg_bid_util': 0.34951377680021933, 'avg_vals_per_issue': 12.25, 'bid_util_std_dev': 0.27261409226599276, 'num_of_bids': 8664, 'num_of_issues': 4, 'weight_std_dev': 0.22930899666170973},
+		"results":{'Agent007': 0.40422609376, 'DreamTeam109Agent': 0.5101678671900001, 'TemplateAgent': 0.41992995121000004, 'GEAAgent': 0.43265635783, 'Agent33': 0.39303834089},
+	},
+	{
+		"features": {'avg_bid_util': 0.3772581513357143, 'avg_vals_per_issue': 8.25, 'bid_util_std_dev': 0.17171122253468077, 'num_of_bids': 3136, 'num_of_issues': 4, 'weight_std_dev': 0.10067082819764621},
+		"results":{'Agent007': 0.5596236255099999, 'DreamTeam109Agent': 0.6049627641499999, 'TemplateAgent': 0.5999174989199999, 'GEAAgent': 0.5224598554400001, 'Agent33': 0.58987276818},
+	},
+	{
+		"features": {'avg_bid_util': 0.37904270749545454, 'avg_vals_per_issue': 10.75, 'bid_util_std_dev': 0.17541450946735304, 'num_of_bids': 5808, 'num_of_issues': 4, 'weight_std_dev': 0.08417367848680489},
+		"results":{'Agent007': 0.47379251338, 'DreamTeam109Agent': 0.6177841223, 'TemplateAgent': 0.47259993294999997, 'GEAAgent': 0.34664827507, 'Agent33': 0.41240778887000007},
+	},
+	{
+		"features": {'avg_bid_util': 0.383723849726923, 'avg_vals_per_issue': 7.166666666666667, 'bid_util_std_dev': 0.18494700568235845, 'num_of_bids': 6552, 'num_of_issues': 6, 'weight_std_dev': 0.14330429159271152},
+		"results":{'Agent007': 0.7280085342000001, 'DreamTeam109Agent': 0.73072819146, 'TemplateAgent': 0.7545044481800001, 'GEAAgent': 0.68948956445, 'Agent33': 0.76318768568},
+	},
+	{
+		"features": {'avg_bid_util': 0.40256737111333335, 'avg_vals_per_issue': 7.4, 'bid_util_std_dev': 0.21297816232145858, 'num_of_bids': 6480, 'num_of_issues': 5, 'weight_std_dev': 0.25429635648195986},
+		"results":{'Agent007': 0.59377700197, 'DreamTeam109Agent': 0.57963818957, 'TemplateAgent': 0.50814773887, 'GEAAgent': 0.16027033927999998, 'Agent33': 0.4912744657},
+	},
+	{
+		"features": {'avg_bid_util': 0.41271624668749995, 'avg_vals_per_issue': 12.25, 'bid_util_std_dev': 0.21335883991888283, 'num_of_bids': 8664, 'num_of_issues': 4, 'weight_std_dev': 0.16338333834880472},
+		"results":{'Agent007': 0.48893929115, 'DreamTeam109Agent': 0.45405610865999996, 'TemplateAgent': 0.36343087691, 'GEAAgent': 0.40254338813999996, 'Agent33': 0.59204294507},
+	},
+	{
+		"features": {'avg_bid_util': 0.43000446364210526, 'avg_vals_per_issue': 5.666666666666667, 'bid_util_std_dev': 0.2010461937334845, 'num_of_bids': 3648, 'num_of_issues': 6, 'weight_std_dev': 0.16182430417654273},
+		"results":{'Agent007': 0.45274927149, 'DreamTeam109Agent': 0.54499842686, 'TemplateAgent': 0.46190764014, 'GEAAgent': 0.31201996707, 'Agent33': 0.51839927751},
+	},
+	{
+		"features": {'avg_bid_util': 0.442903329425, 'avg_vals_per_issue': 8.25, 'bid_util_std_dev': 0.18623253291019665, 'num_of_bids': 3136, 'num_of_issues': 4, 'weight_std_dev': 0.22484759682949693},
+		"results":{'Agent007': 0.6784075242000001, 'DreamTeam109Agent': 0.6485335438, 'TemplateAgent': 0.60360580868, 'GEAAgent': 0.50994666364, 'Agent33': 0.65918928688},
+	},
+	{
+		"features": {'avg_bid_util': 0.45508033748131865, 'avg_vals_per_issue': 7.166666666666667, 'bid_util_std_dev': 0.2529000779480297, 'num_of_bids': 6552, 'num_of_issues': 6, 'weight_std_dev': 0.13814583039511383},
+		"results":{'Agent007': 0.69916013702, 'DreamTeam109Agent': 0.81047655338, 'TemplateAgent': 0.8189699420800001, 'GEAAgent': 0.7681652555400001, 'Agent33': 0.7958946489800001},
+	},
+	{
+		"features": {'avg_bid_util': 0.46815419990624996, 'avg_vals_per_issue': 6.8, 'bid_util_std_dev': 0.20621158819521093, 'num_of_bids': 4608, 'num_of_issues': 5, 'weight_std_dev': 0.1532983094492565},
+		"results":{'Agent007': 0.77937213944, 'DreamTeam109Agent': 0.77666147672, 'TemplateAgent': 0.9016926635899999, 'GEAAgent': 0.73795378074, 'Agent33': 0.89328275},
+	},
+	{
+		"features": {'avg_bid_util': 0.49260216050701755, 'avg_vals_per_issue': 5.666666666666667, 'bid_util_std_dev': 0.19969045740842725, 'num_of_bids': 3648, 'num_of_issues': 6, 'weight_std_dev': 0.11899694781333213},
+		"results":{'Agent007': 0.47343298335000006, 'DreamTeam109Agent': 0.57929851409, 'TemplateAgent': 0.42661414302999995, 'GEAAgent': 0.31058299452, 'Agent33': 0.5876510938800001},
+	},
+]
