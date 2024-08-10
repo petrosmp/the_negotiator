a2a5f72073a83731c2864e0f3ac5f732b305a72f
Ioannis Christofilogiannis <ichristofilogian@tuc.gr>
2024-08-10 17:08:42 +0300

    changed README, report title, folder name, added summary

--- ANL-2023-example-agent/agents/ANL2022/micro_agent/party.py
+++
@@ -1,8 +0,0 @@
-'''
-Created on Apr 21, 2022
-
-@author: Dave de Jonge, IIIA-CSIC
-'''
-from micro_agent.micro_agent import MiCROAgent
-def party():
-    return MiCROAgent
--- ANL-2023-example-agent/agents/the_negotiator/arsenal/dreamteam109_agent/utils/utils.py
+++
@@ -1,4 +0,0 @@
-from geniusweb.issuevalue.Bid import Bid
-
-def bid_to_string(bid: Bid) -> str:
-    return str(dict(sorted(bid.getIssueValues().items())))
